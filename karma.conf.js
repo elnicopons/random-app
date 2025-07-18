6ffb934cce6f36f2015985a9927bd6bf201bac6c
Nico <elnicopons@gmai.com>
2025-07-18 19:34:48 -0300

    Fixes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/elnicopons'),
+      dir: require('path').join(__dirname, './coverage/random-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'elnicopons'`, () => {
+  it(`should have as title 'random-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('elnicopons');
+    expect(app.title).toEqual('random-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('elnicopons app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('random-app app is running!');
   });
 });
